e7f4dec4abd02e928d3b8c70654990215c6971aa
AmazonPython <amazon3@foxmail.com>
2020-11-19 14:44:37 +0800

    Optimized the UI of the beep page and hanged tthe 404 page to make it more beautiful

--- resources/views/publish_beep_panel.blade.php
+++ resources/views/publish_beep_panel.blade.php
@@ -18,7 +18,18 @@
         <footer class="flex justify-between items-center">
             <img src="{{ auth()->user()->avatar }}" alt="your avatar" class="rounded-full mr-2" width="50" height="50">
 
-            <input class="hover:bg-blue-300 rounded-lg shadow px-8 text-m text-purple-600" type="file" name="picture" id="picture" accept="image/*">
+            <input class=" hidden"
+                   type="file"
+                   name="picture"
+                   id="picture"
+                   accept="image/*"
+            >
+            <label for="picture" class="z-20 flex flex-col-reverse items-center justify-center w-full h-full cursor-pointer">
+                <p class="text-s text-blue-900">Click here to upload pictures</p>
+                <svg class="z-10 w-8 h-8 text-indigo-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"></path>
+                </svg>
+            </label>
 
             @error('picture')
                 <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
